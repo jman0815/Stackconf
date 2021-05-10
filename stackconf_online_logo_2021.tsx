860fc3a4cc4d06b3bb4c862fd72249726aa6922c
jhartmann44 <jhartmann@netways.de>
2021-05-10 09:47:16 +0200

    Stackconf logo

---
+++ stackconf_online_logo_2021.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="stackconf_online_logo_2021" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="stackconf_online_logo_2021.png" width="128" height="108"/>
+</tileset>
