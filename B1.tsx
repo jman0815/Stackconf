4821b63546c8a1acf8da5c0824bba08e4a09697a
jhartmann44 <jhartmann@netways.de>
2021-05-11 12:53:04 +0200

    bug fixes

---
+++ B1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="B1" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="B1.png" width="96" height="64"/>
+</tileset>
