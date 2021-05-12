3e629553b71169daff5ece5d0fc0ff527d65283b
jhartmann44 <jhartmann@netways.de>
2021-05-12 15:31:27 +0200

    new stuff

---
+++ BreakoutSession.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="BreakoutSession" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="BreakoutSession.png" width="128" height="85"/>
+</tileset>
---
+++ Session3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Session3" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="Session3.png" width="96" height="64"/>
+</tileset>
