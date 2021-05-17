7504eb087aada9d23091243247c8321447e4dbc3
jhartmann44 <jhartmann@netways.de>
2021-05-17 10:33:12 +0200

    rm Rooms

--- Room4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Room4" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="Room4.png" width="96" height="64"/>
-</tileset>
